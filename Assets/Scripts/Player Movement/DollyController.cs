a8e3425a1f847d80852f8b9ea55a2d361627a35a
d-guzman <dghost121@gmail.com>
2020-02-27 22:42:17 -0800

    Moved stuff around in asset folder
    --- Changes ---
    - The scripts in the asset folder have been better organized. Each script is placed in to a folder named after the thing the script interacts with.
    
    --- Notes ---
    - lmao i forgot to push this update so when i came back like 3 months later to review my code, i saw 61 changes and freaked out a bit.
    - tried to do an experiment with multiple tracks being cinemachine didn't like that. might review the multiple tracks idea, if i change up the camera.

---
+++ Assets/Scripts/Player Movement/DollyController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollyController : MonoBehaviour
+{
+    public Cinemachine.CinemachineDollyCart dollyCartScript;
+    public bool isBoosting;
+    public bool isBraking;
+
+    public float defaultSpeed;
+    public float boostSpeed;
+    public float brakeSpeed;
+
+    /*
+    [Header("Testing Track Swapping")]
+    public bool SwapToTrack2 = false;
+    public Vector3 swapPoint;
+    public Cinemachine.CinemachinePathBase primaryTrack;
+    public Cinemachine.CinemachinePathBase secondaryTrack;
+    private float positionUnitsBeforeSwap;
+    [SerializeField]
+    private bool ifSwapFinished = false;
+    */
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (isBoosting)
+            dollyCartScript.m_Speed = boostSpeed;
+        else if (isBraking)
+            dollyCartScript.m_Speed = brakeSpeed;
+        else
+            dollyCartScript.m_Speed = defaultSpeed;
+
+        /*
+        float distanceFromSwapPoint = Vector3.Distance(transform.position, swapPoint);
+        if (distanceFromSwapPoint < 20f)
+            Debug.Log("Close to Swap Point!");
+
+        
+        if (SwapToTrack2)
+        {
+            if (dollyCartScript.m_Path != secondaryTrack)
+            {
+                ifSwapFinished = false;
+                if (distanceFromSwapPoint < 10f)
+                {
+                    positionUnitsBeforeSwap = dollyCartScript.m_Position;
+                    if (!ifSwapFinished)
+                    {
+                        dollyCartScript.m_Position = 0;
+                        dollyCartScript.m_Path = secondaryTrack;
+                        ifSwapFinished = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (dollyCartScript.m_Path != primaryTrack)
+            {
+                ifSwapFinished = false;
+                if (distanceFromSwapPoint < 10f)
+                {
+                    if (!ifSwapFinished)
+                    {
+                        dollyCartScript.m_Position = positionUnitsBeforeSwap;
+                        dollyCartScript.m_Path = primaryTrack;
+                        ifSwapFinished = true;
+                    }
+                }
+            }
+        }
+        */
+    }
+}
--- Assets/Scripts/Player Movement/PlayerController.cs
+++ Assets/Scripts/Player Movement/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TestPlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Variables for Ship Management.
     public PlayerShipData[] shipData;
